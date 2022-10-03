4e2dc1d66c20067337caf7d43bdc430ce8065e65
ecchi1234 <ngocchi.fit148.vnu@gmail.com>
2022-10-03 16:00:06 +0700

    ssg learning

--- pages/posts/[postId].tsx
+++ pages/posts/[postId].tsx
@@ -1,16 +1,49 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
 import { useRouter } from 'next/router';
 import * as React from 'react';
 
-export interface PostDetailPageProps {}
+export interface PostDetailPageProps {
+  post: any;
+}
 
-export default function PostDetailPage(props: PostDetailPageProps) {
+export default function PostDetailPage({ post }: PostDetailPageProps) {
   const router = useRouter();
+
+  if (!post) return null;
 
   return (
     <div>
       <h1>Post Detail Page</h1>
 
-      <div>Query: {JSON.stringify(router.query)}</div>
+      <div>{post.title}</div>
+      <div>{post.author}</div>
+      <div>{post.description}</div>
     </div>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await fetch('https://js-post-api.herokuapp.com/api/posts?_page=1');
+  const data = await response.json();
+  return {
+    paths: data.data.map((post: any) => ({ params: { postId: post.id } })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<PostDetailPageProps> = async (
+  context: GetStaticPropsContext
+) => {
+  // get post id from context params
+  const postId = context.params?.postId;
+  if (!postId) {
+    return { notFound: true };
+  }
+  const response = await fetch(`https://js-post-api.herokuapp.com/api/posts/${postId}`);
+  const data = await response.json();
+  return {
+    props: {
+      post: data,
+    },
+  };
+};
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,38 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next';
 import * as React from 'react';
 
-export interface PostListPageProps {}
-
-export default function PostListPage(props: PostListPageProps) {
-  return <div>Post List Page</div>;
+export interface PostListPageProps {
+  posts: any[];
 }
+
+export default function PostListPage({ posts }: PostListPageProps) {
+  return (
+    <div>
+      <h1>Post List Page</h1>
+      <div>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+export const getStaticProps: GetStaticProps<PostListPageProps> = async (
+  context: GetStaticPropsContext
+) => {
+  // run on server
+  // at build time
+
+  // call api:
+  const response = await fetch('https://js-post-api.herokuapp.com/api/posts?_page=1');
+  const data = await response.json();
+  // testing with this console.log
+  console.log(' static site generation');
+  return {
+    props: {
+      posts: data.data.map((post: any) => ({ id: post.id, title: post.title })),
+    },
+  };
+};
